30daada646ac3692dbc7a9e914338c442b9e309b
Olga Mazurina <olya_28_94@mail.ru>
2020-10-01 20:27:22 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if ( array == null || array.length === 0 ) {return 0;}
+    let n = array[0];
+    for (let i = 1; array.length > i; i++) {
+        if (n >= array[i]) {
+            n = array[i];
+        }
 
-exports.min = function min (array) {
-  return 0;
-}
+    }
+    return n;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if ( array == null || array.length === 0 ) {return 0;}
+    let n = array[0];
+    for (let i = 1; array.length > i; i++) {
+        if (n <= array[i]) {
+            n = array[i];
+        }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    }
+    return n;
+};
+
+exports.avg = function avg(array) {
+    if ( array == null || array.length === 0 ) {return 0;}
+    let sum = 0;
+        for (let i = 0; array.length > i; i++ ) {
+        sum += array[i];
+    }
+
+    let av = sum / array.length;
+
+
+
+    return av;
+};
